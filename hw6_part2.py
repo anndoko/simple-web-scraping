4acf9b9b005311afbe46fdeee0a156aafd56b25f
Anndo Ko <handcuffanndo@gmail.com>
2018-02-12 14:57:15 -0500

    finish part2: get the headlines

--- hw6_part2.py
+++ hw6_part2.py
@@ -8,3 +8,12 @@ print('\n*********** PART 2 ***********')
 print('Michigan Daily -- MOST READ\n')
 
 ### Your Part 2 solution goes here
+html = requests.get("https://www.michigandaily.com/").text
+soup = BeautifulSoup(html, "html.parser")
+# print(soup.prettify())
+
+most_read = soup.find(class_="view-id-most_read")
+top_articles = most_read.find_all("a")
+
+for article in top_articles:
+    print(article.text)
