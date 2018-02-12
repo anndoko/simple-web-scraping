3d05eb710e371cc0dd7519481d0c994cf250f8e0
Anndo Ko <handcuffanndo@gmail.com>
2018-02-12 13:59:36 -0500

    use sys to get input

--- hw6_part1.py
+++ hw6_part1.py
@@ -1,13 +1,16 @@
 # 507/206 Homework 6 Part 1
 import requests
 from bs4 import BeautifulSoup
+import sys
 
 #### Part 1 ####
 print('\n*********** PART 1 ***********')
 print("Mark's page -- Alt tags\n")
 
+input_link = sys.argv[1]
+
 ### Your Part 1 solution goes here
-html = requests.get("http://newmantaylor.com/gallery.html").text
+html = requests.get(input_link).text
 soup = BeautifulSoup(html, "html.parser")
 # print(soup.prettify())
 
