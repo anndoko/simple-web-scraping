7bd17d4489bace0e085bbb31db62b2dd909c5062
Anndo Ko <handcuffanndo@gmail.com>
2018-02-12 13:55:37 -0500

    add code to get alt

--- hw6_part1.py
+++ hw6_part1.py
@@ -7,3 +7,14 @@ print('\n*********** PART 1 ***********')
 print("Mark's page -- Alt tags\n")
 
 ### Your Part 1 solution goes here
+html = requests.get("http://newmantaylor.com/gallery.html").text
+soup = BeautifulSoup(html, "html.parser")
+# print(soup.prettify())
+
+img_tags = soup.find_all("img")
+
+for img in img_tags:
+    try:
+        print(img["alt"])
+    except:
+        print("No alternative text provided!!")
